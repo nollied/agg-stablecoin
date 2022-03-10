61f31890bd27853b34e7f820095b4aa9d49a980f
McCrearyD <mccreary@dyllan.ai>
2022-03-09 21:05:58 -0800

    remove unused code

--- plot_data.py
+++ plot_data.py
@@ -6,13 +6,6 @@ from functools import reduce
 from collections import defaultdict
 
 
-def plot_currency(ticker):
-    field = 'high'
-    df = pd.read_csv(get_path(ticker))
-    df.plot(x='dt', y=field)
-    plt.show()
-
-
 def plot_averages(title, cryptos=CRYPTOS, normalize=False, moving_avg_window=1):
     # TODO: docstring
 
@@ -37,14 +30,16 @@ def plot_averages(title, cryptos=CRYPTOS, normalize=False, moving_avg_window=1):
 
     # calculate Y's moving average
     if moving_avg_window > 1:
-        Y_avg = []
-        for i in range(len(Y)):
-            if i >= moving_avg_window:
-                Y_avg.append(np.mean(Y[i - moving_avg_window:i]))
+        Y_avg = [
+            np.mean(Y[i - moving_avg_window : i])
+            for i in range(len(Y))
+            if i >= moving_avg_window
+        ]
+
         plt.plot(range(len(Y_avg)), Y_avg)
     else:
         plt.plot(range(len(X)), Y)
-    
+
     plt.xlabel(f'Days between {START_DATE} and {END_DATE}')
     plt.ylabel('Price (USD)')
     plt.title(title)
@@ -57,4 +52,3 @@ if __name__ == "__main__":
     plot_averages('$BTC', ['BTC'])
     plot_averages('$AGG', moving_avg_window=1, normalize=True)
     plot_averages('$AGG (normalized + 1-year moving average)', moving_avg_window=365, normalize=True)
-
